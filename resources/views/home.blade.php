733b21fb0702dc0d4ae1be8d890b4cd916551109
Johnson Sebire <johnson@manifestghana.com>
2023-01-09 09:34:43 +0000

    Updated Homepage Content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,13 @@
 <section class="about-us-area about-3-area">
     <div class="container">
         <div class="row align-items-center position-relative">
-            <div class="col-lg-7">
+            <div class="col-lg-8">
                 <div class="about-content">
                     <span class=" wow fadeInLeft animated" data-wow-duration="1000ms" data-wow-delay="100ms">Battery Energy Storage Systems</span>
                     <h3 class="title wow fadeInLeft animated" data-wow-duration="1000ms" data-wow-delay="300ms">
 
-                        Battery Energy Storage Systems
-                        
-                        + Financing Solutions
+                        Battery Energy <br />Storage Systems <br> + Financing 
+                        Solutions
                         </h3>
                     <p class=" wow fadeInLeft animated" data-wow-duration="1000ms" data-wow-delay="500ms">
                         We offer battery solutions that enable cost savings and revenues for a wide range of market segments. Together with financing solutions such as leasing and battery-as-a-service we can ensure that everyone can become more cost efficient and enjoy the possibilities of utilizing a battery energy storage system (BESS).</p>
