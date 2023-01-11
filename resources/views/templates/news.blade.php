181d81a43d50aafe4eb4066f8df509c072d83ca3
Johnson Sebire <johnson@manifestghana.com>
2023-01-11 09:20:43 +0000

    Scaffolding Blog Area

--- resources/views/templates/news.blade.php
+++ resources/views/templates/news.blade.php
@@ -2,5 +2,101 @@
 
     <x-page-title :title="$page->title" />
 
-        <x-cta-phone /> 
+
+
+    @collection('articles', ['limit' => 10, 'paginate' => true])
+            @foreach($entry['entries'] as $entry)
+            
+            {{-- @dd($entry) --}}
+
+            @data($entry)
+        
+        <!--====== BLOG STANDARD PART START ======-->
+         @if (!isset($featured_image))
+         <style> 
+            .blog-standard .single-blog-standard .blog-standard-content {
+                border:2px solid #ededed !important; 
+            }
+            </style> 
+         @endif
+    <div class="blog-standard-area pt-90 pb-120">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <div class="blog-standard" >
+                        <div class="single-blog-standard mt-30" >
+                            @isset($featured_image)
+                            <div class="blog-standard-thumb">
+                                {{-- @dd($featured_image) --}}
+                                <img src="{{$featured_image['url']}}" alt="blog">
+                            </div>
+                            @endisset
+                            <div class="blog-standard-content">
+                                <span>{{$blueprint['title']}}</span>
+                                <h2 class="title">{{ $title }} </h2>
+                                <ul>
+                                    {{-- <li><i class="fal fa-eye"></i> 232 Views</li> --}}
+                                    {{-- <li><i class="fal fa-comments"></i> 35 Comments</li> --}}
+                                    <li><i class="fal fa-calendar-alt"></i> {{$updated_at}} </li>
+                                </ul>
+                                <p>{!! Statamic::modify($content)->safeTruncate([250, '...']) !!}</p>
+                                <div class="blog-flex">
+                                    
+                                    {{-- @dd($entry) --}}
+
+                                    @isset($updated_by['name'])
+                                        <div class="blog-left" style="padding-left:0 !important">
+                                            <p><span>Author:</span> {{$updated_by['name'] }}</p>
+                                            {{-- <img src="assets/images/user.png" alt=""> --}}
+                                        </div>
+                                    @endisset
+                                    <div class="blog-right">
+                                        <a href="{{$url}}"><i class="fal fa-arrow-right"></i>Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @enddata
+                        @endforeach
+                    @endcollection
+                    </div>
+                  
+
+                    {{-- <div class="row">
+                        <div class="col-lg-12">
+                            <div class="pagination-area d-flex justify-content-center mt-40">
+                                <nav aria-label="Page navigation example">
+                                    <ul class="pagination">
+                                        <li class="page-item">
+                                            <a class="page-link" href="#" aria-label="Previous">
+                                                <span aria-hidden="true">&laquo;</span>
+                                            </a>
+                                        </li>
+                                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">...</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">10</a></li>
+                                        <li class="page-item">
+                                            <a class="page-link" href="#" aria-label="Next">
+                                                <span aria-hidden="true">&raquo;</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div> --}}
+                </div>
+
+      
+                
+            </div>
+        </div>
+    </div> 
+
+    <!--====== BLOG STANDARD PART ENDS ======-->
+
+    <x-cta-phone /> 
+
 </x-frontend-layout>
--- resources/views/templates/portfolio.blade.php
+++ resources/views/templates/portfolio.blade.php
@@ -1,5 +1,186 @@
 <x-frontend-layout :context="$__data">
 
     <x-page-title :title="$page->title" />
-        <x-cta-phone /> 
+
+        <!--====== GALLERY PART START ======-->
+
+    <div class="gallery-area pt-90 pb-120">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="tab-btns d-flex justify-content-center">
+                        <ul class="nav nav-pills" id="pills-tab" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" id="pills-1-tab" data-toggle="pill" href="#pills-1" role="tab" aria-controls="pills-1" aria-selected="true">Sweden</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="pills-2-tab" data-toggle="pill" href="#pills-2" role="tab" aria-controls="pills-2" aria-selected="false">Africa</a>
+                            </li>
+                            
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="tab-content" id="pills-tabContent">
+                <div class="tab-pane fade show active" id="pills-1" role="tabpanel" aria-labelledby="pills-1-tab">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-4 col-md-6 col-sm-8">
+                            <div class="gallery-item mt-15">
+                                <img src="assets/images/gallery-1.jpg" alt="gallery">
+                                <div class="gallery-overlay">
+                                    <a class="image-popup" href="assets/images/gallery-1.jpg"><i class="fal fa-expand"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-8">
+                            <div class="gallery-item mt-15">
+                                <img src="assets/images/gallery-2.jpg" alt="gallery">
+                                <div class="gallery-overlay">
+                                    <a class="image-popup" href="assets/images/gallery-2.jpg"><i class="fal fa-expand"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-8">
+                            <div class="gallery-item mt-15">
+                                <img src="assets/images/gallery-3.jpg" alt="gallery">
+                                <div class="gallery-overlay">
+                                    <a class="image-popup" href="assets/images/gallery-3.jpg"><i class="fal fa-expand"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-8">
+                            <div class="gallery-item mt-15">
+                                <img src="assets/images/gallery-4.jpg" alt="gallery">
+                                <div class="gallery-overlay">
+                                    <a class="image-popup" href="assets/images/gallery-4.jpg"><i class="fal fa-expand"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-8">
+                            <div class="gallery-item mt-15">
+                                <img src="assets/images/gallery-5.jpg" alt="gallery">
+                                <div class="gallery-overlay">
+                                    <a class="image-popup" href="assets/images/gallery-5.jpg"><i class="fal fa-expand"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-8">
+                            <div class="gallery-item mt-15">
+                                <img src="assets/images/gallery-6.jpg" alt="gallery">
+                                <div class="gallery-overlay">
+                                    <a class="image-popup" href="assets/images/gallery-6.jpg"><i class="fal fa-expand"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-8">
+                            <div class="gallery-item mt-15">
+                                <img src="assets/images/gallery-7.jpg" alt="gallery">
+                                <div class="gallery-overlay">
+                                    <a class="image-popup" href="assets/images/gallery-7.jpg"><i class="fal fa-expand"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-8">
+                            <div class="gallery-item mt-15">
+                                <img src="assets/images/gallery-8.jpg" alt="gallery">
+                                <div class="gallery-overlay">
+                                    <a class="image-popup" href="assets/images/gallery-8.jpg"><i class="fal fa-expand"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-8">
+                            <div class="gallery-item mt-15">
+                                <img src="assets/images/gallery-9.jpg" alt="gallery">
+                                <div class="gallery-overlay">
+                                    <a class="image-popup" href="assets/images/gallery-9.jpg"><i class="fal fa-expand"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="pills-2" role="tabpanel" aria-labelledby="pills-2-tab">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-4 col-md-6 col-sm-8">
+                            <div class="gallery-item mt-15">
+                                <img src="assets/images/gallery-1.jpg" alt="gallery">
+                                <div class="gallery-overlay">
+                                    <a class="image-popup" href="assets/images/gallery-1.jpg"><i class="fal fa-expand"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-8">
+                            <div class="gallery-item mt-15">
+                                <img src="assets/images/gallery-2.jpg" alt="gallery">
+                                <div class="gallery-overlay">
+                                    <a class="image-popup" href="assets/images/gallery-2.jpg"><i class="fal fa-expand"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-8">
+                            <div class="gallery-item mt-15">
+                                <img src="assets/images/gallery-3.jpg" alt="gallery">
+                                <div class="gallery-overlay">
+                                    <a class="image-popup" href="assets/images/gallery-3.jpg"><i class="fal fa-expand"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-8">
+                            <div class="gallery-item mt-15">
+                                <img src="assets/images/gallery-4.jpg" alt="gallery">
+                                <div class="gallery-overlay">
+                                    <a class="image-popup" href="assets/images/gallery-4.jpg"><i class="fal fa-expand"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-8">
+                            <div class="gallery-item mt-15">
+                                <img src="assets/images/gallery-5.jpg" alt="gallery">
+                                <div class="gallery-overlay">
+                                    <a class="image-popup" href="assets/images/gallery-5.jpg"><i class="fal fa-expand"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-8">
+                            <div class="gallery-item mt-15">
+                                <img src="assets/images/gallery-6.jpg" alt="gallery">
+                                <div class="gallery-overlay">
+                                    <a class="image-popup" href="assets/images/gallery-6.jpg"><i class="fal fa-expand"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-8">
+                            <div class="gallery-item mt-15">
+                                <img src="assets/images/gallery-7.jpg" alt="gallery">
+                                <div class="gallery-overlay">
+                                    <a class="image-popup" href="assets/images/gallery-7.jpg"><i class="fal fa-expand"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-8">
+                            <div class="gallery-item mt-15">
+                                <img src="assets/images/gallery-8.jpg" alt="gallery">
+                                <div class="gallery-overlay">
+                                    <a class="image-popup" href="assets/images/gallery-8.jpg"><i class="fal fa-expand"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-8">
+                            <div class="gallery-item mt-15">
+                                <img src="assets/images/gallery-9.jpg" alt="gallery">
+                                <div class="gallery-overlay">
+                                    <a class="image-popup" href="assets/images/gallery-9.jpg"><i class="fal fa-expand"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                
+            </div>
+        </div>
+    </div>
+
+    <!--====== GALLERY PART ENDS ======-->
+
+    <x-cta-phone /> 
 </x-frontend-layout>
